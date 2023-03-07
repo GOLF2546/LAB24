409968876f52419bd1956510757403b7ea0621c9
Golf <Pattarapolnanakorn@gmail.com>
2023-03-07 16:34:16 +0700

    Finish

--- lab24.cpp
+++ lab24.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+#define PI 3.14159265
 
 class ComplexNumber{				
 	public:
@@ -29,6 +30,70 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+
+ComplexNumber operator+(double x,ComplexNumber c){
+	return ComplexNumber(x+c.real,c.imag);
+}
+
+ComplexNumber operator-(double x,ComplexNumber c){
+	return ComplexNumber(x-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,real*c.imag+imag*c.real);
+}
+
+ComplexNumber operator*(double x,ComplexNumber c){
+	return ComplexNumber(x*c.real,x*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((real*c.real+imag*c.imag)/(pow(c.real,2)+pow(c.imag,2)),(imag*c.real-real*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ComplexNumber operator/(double x,ComplexNumber c){
+	return ComplexNumber((x*c.real)/(pow(c.real,2)+pow(c.imag,2)),(-x*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real)*180/PI;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+bool operator==(double x,ComplexNumber c){
+	if(x == c.real && c.imag == 0) return true;
+	else return false;
+}
+
+ostream & operator<<(ostream &os,const ComplexNumber &c){
+	if(c.real != 0){
+		if(c.imag>0){
+			return os << c.real << "+" << c.imag << "i";
+		}else if(c.imag<0){
+			return os << c.real << c.imag << "i";
+		}else{
+			return os << c.real;
+		}
+	}else{
+		if(c.imag>0){
+			return os << c.imag << "i";
+		}else if(c.imag<0){
+			return os << c.imag << "i";
+		}else{
+			return os << c.real;
+		}
+	}
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
